80788d8b9cde584e90050f6895c28758c34b1b2e
Yaroslav Litvinov <yarulik1986@gmail.com>
2021-08-02 17:20:18 +0500

    rename

--- engix_robot/scripts/start.sh
+++ engix_robot/scripts/start.sh
@@ -4,4 +4,3 @@ ip4=$(/sbin/ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
 export ROS_IP=$ip4
 export ROS_IP
 roslaunch realsense2_camera opensource_tracking.launch
-##roslaunch ros_odrive position.launch
