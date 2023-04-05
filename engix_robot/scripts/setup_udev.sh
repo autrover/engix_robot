bdffca995070eb327fe54af4fdab62df95b26e8b
xavier <yarulik1986@gmail.com>
2023-04-05 18:45:46 +0100

    big update, tune nodes, Yarik

--- engix_robot/scripts/setup_udev.sh
+++ engix_robot/scripts/setup_udev.sh
@@ -3,6 +3,6 @@ echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="1209", ATTR{idProduct}=="0d[0-9][0-9]",
 echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0777", SYMLINK+="rplidar"' | sudo tee /etc/udev/rules.d/92-rplidar.rules
 echo 'KERNEL=="js*", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c21f", SYMLINK="input/f710"'  | sudo tee /etc/udev/rules.d/93-logitechf710.rules
 echo 'KERNEL=="js*", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c21d", SYMLINK="input/f310"'  | sudo tee /etc/udev/rules.d/94-logitechf310.rules
-
+echo 'KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="0483", MODE="0666",  SYMLINK+="sonar"' | sudo tee /etc/udev/rules.d/49-teensy.rules
 sudo udevadm control --reload-rules
 sudo udevadm trigger
